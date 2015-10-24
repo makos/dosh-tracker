04974f3f07061bb8e54a8d6f9f138690a73865cd
makos <matmakos@gmail.com>
2015-10-25 00:13:30 +0200

    added comments on what to do next, added set_options(), updated README

--- jsonhandler.py
+++ jsonhandler.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
 import json
@@ -6,25 +7,20 @@ class JSONHandler():
     """Small wrapper for JSON encoding/decoding.
     Handles only Python lists, but adding other types should be ez.
     
-    Constructor takes 1 argument, `db`, which should be a string containing JSON database file name.
+    Constructor takes 1 argument, 'db', which should be a string containing JSON database file name.
     If file doesn't exist, it will be created with empty JSON array.
     
     Methods:
         load - takes no arguments, decodes JSON and returns it (a list)
-        dump - takes `data` argument (a list), encodes it and saves to file
-        clear - wipes the `db` file"""
+        dump - takes 'data' argument (a list), encodes it and saves to file
+        clear - wipes the 'db' file"""
 
-    json_data = []
+    json_data = [] # this is ugly
     def __init__(self, db):
         if type(db) != str:
             print("JSONHandler: Invalid argument; needs to be file name in string format")
         else:
             self.db = db
-        # data = self.load(db)
-        # if data == None:
-        #     self.dump([], db)
-        # JSONHandler.json_data = data
-        # self.dummy()
 
     def load(self):
         try:
@@ -33,25 +29,28 @@ class JSONHandler():
             json_file.close()
             return JSONHandler.json_data
         except FileNotFoundError:
-            print("JSONHandler: No JSON file with name", self.db, "found.")
-            return None
-
+            print("JSONHandler: No JSON file with name", self.db, "found.") # this should be remade so the file is
+            return None                                                     # generated automatically - easy, but won't
+                                                                            # it break parts of code in main that use it?
     def dump(self, data):
         JSONHandler.json_data.append(data)
         json_file = open(self.db, 'w')
-        json.dump(JSONHandler.json_data, json_file)
+        json.dump(JSONHandler.json_data, json_file, indent = 4)
         json_file.close()
         #print("JSONHandler: Data dumped into", str(self.db))
 
-    def clear(self):
-        option = input("JSONHandler: Database wipe - do you really want to do this? y/n")
-        if option == 'y':
+    def clear(self, prompt = True):
+        if prompt:
+            option = input("JSONHandler: Database wipe - do you really want to do this? y/n ")
+            if option == 'y':
+                JSONHandler.json_data = []
+                json_file = open(self.db, 'w')
+                json.dump(JSONHandler.json_data, json_file)
+                json_file.close()
+                print("JSONHandler: Database erased")
+        else:
             JSONHandler.json_data = []
             json_file = open(self.db, 'w')
             json.dump(JSONHandler.json_data, json_file)
             json_file.close()
             print("JSONHandler: Database erased")
-
-# if __name__ == '__main__':
-#     prog = JSONHandler
-#     prog()
--- main.py
+++ main.py
@@ -4,11 +4,9 @@
 # A tool to record expenditures, with crude planning features
 # by makos
 # TODO
-# - backbone structure of the program
-# - database (json)
-# - add conf file to set range of display (month, week)
-# - add sum of spendings in given range
-# - ... 
+# - make the code more generalized
+# - clean the code up
+# - fix some hacks to be prettier
 # - GUI
 
 from jsonhandler import JSONHandler
@@ -22,6 +20,7 @@ STEP = 2678400.0 # default cutoff for entry display - one month
 CALC_RATIOS = (0.7, 0.2, 0.1) # ratios used for savings calculator
 # first is living costs, second is savings, third is luxury items (not necessary for living)
 DEFAULTS = [["step", STEP], ["calc_ratios", CALC_RATIOS]]
+CONFIG = ".config"
 
 def add_entry():
     # Entry fields:
@@ -64,9 +63,9 @@ def calculator():
     print("Money for savings account:", savings)
     print("Disposable money for luxury items:", luxury)
 
-def read_config(cfg_file=".config"):
-    global STEP, CALC_RATIOS, DEFAULTS
-    cfg_handler = JSONHandler(cfg_file)
+def read_config():
+    global STEP, CALC_RATIOS, DEFAULTS, CONFIG
+    cfg_handler = JSONHandler(CONFIG)
     cfg_contents = cfg_handler.load()
     
     if cfg_contents == None:
@@ -78,6 +77,35 @@ def read_config(cfg_file=".config"):
                     STEP = option[1]
                 elif option[0] == "calc_ratios":
                     CALC_RATIOS = option[1]
+
+def set_options():
+    # this whole function seems ugly and it looks like there's
+    # a better way to do this but I still need to come up with it
+    global DEFAULTS, CONFIG, STEP, CALC_RATIOS
+    cfg_handler = JSONHandler(CONFIG)
+    print("opening", CONFIG, "in handler")
+    print("\nAvailable settings:")
+    option = input("Step - decides what range is used to display entries - can be (1), (7) or (31) days > ")
+    if option == '1':
+        STEP = 86400 # 24 hours
+    elif option == '7':
+        STEP = 604800 # a week
+    elif option == '31':
+        STEP = 2678400 # a month
+
+    option = input("Ratios - decides what ratios are used for savings calculator, input in form of 3 integers that sum up to 1.0 > ")
+    option = option.split()
+    if len(option) == 3:
+        helper_list = []
+        for opt in option:
+            helper_list.append(float(opt))
+        CALC_RATIOS = helper_list
+
+    cfg_handler.clear(prompt = False)
+    DEFAULTS = [["step", STEP], ["calc_ratios", CALC_RATIOS]] # especially this part is ugly
+    cfg_handler.dump(DEFAULTS)
+    print("Settings saved.")
+
 def show_help():
     print("Usage: python3 main.py [JSON file name] [-c -h] [config file name]")
     print("\nProviding JSON and config files is optional, defaults are created automatically.")
@@ -87,7 +115,7 @@ def show_help():
 
 def main():
     while True:
-        option = input("\n(A)dd an entry, (V)iew entries, (S)earch entries, Savings (c)alculator, (Q)uit\n")
+        option = input("\n(A)dd an entry, (V)iew entries, (S)earch entries, Savings (c)alculator, (O)ptions, (Q)uit\n")
 
         if type(option) == str:
             option.lower()
@@ -101,6 +129,8 @@ def main():
                 search()
             elif option == 'c':
                 calculator()
+            elif option == 'o':
+                set_options()
             elif option == 'debug':
                 global STEP, CALC_RATIOS
                 print("STEP = ", STEP)
@@ -108,39 +138,23 @@ def main():
             elif option == 'clear':
                 j_handler.clear()
             else:
-                pass #XXX
+                pass
 
 if len(argv) > 1:
     if ".json" in argv[1]:
         j_handler = JSONHandler(argv[1])
         if len(argv) > 2:
             if (argv[2] == "-c") and (len(argv) > 3):
-                read_config(argv[3])
-            else:
-                read_config()
-        else:
-            read_config()
+                CONFIG = argv[3]
     elif argv[1] == "-h":
         show_help()
     elif (argv[1] == "-c") and (len(argv) > 2):
         j_handler = JSONHandler("db.json")
-        read_config(argv[2])
+        CONFIG = argv[2]
     else:
         show_help()
 else:
     j_handler = JSONHandler("db.json")
-    read_config()
 
-# testing stuff
-# j_handler.dump([1442955507.7310047, 'old shit', 5.55])
-# j_handler.dump([1445633908.7310047, 'new shit', 4.44])
-# handler = JSONHandler('db.json')
-# someshit = ['dingus', 'crap', 555]
-# print(handler.load())
-# handler.dump(someshit)
-# data = handler.load()
-# print(handler.load())
-# handler.clear()
-# print(handler.load())
-
+read_config()
 main()
